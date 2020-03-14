bf5068d0739615fa64f6abf2f1c02aa758d7fce2
Michael Ballantyne <michael.ballantyne@gmail.com>
2020-03-14 18:40:30 -0400

    add a build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+export CXXFLAGS=-std=c++11
+MINC=../minisat-build/install/include
+MLIB=../minisat-build/install/lib
+MPREFIX=install
+
+make config MINISAT_INCLUDE=-I$MINC
+make config MINISAT_LIB="-L$MLIB -lminisat"
+make config prefix=$MPREFIX
+make install
