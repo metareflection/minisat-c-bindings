2e2e206133733f150dcca66fc013dade20e08535
Michael Ballantyne <michael.ballantyne@gmail.com>
2020-04-07 17:11:44 -0400

    fix build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 export CXXFLAGS=-std=c++11
-MINC=../minisat-build/install/include
-MLIB=../minisat-build/install/lib
+MINC=../minisat/build/install/include
+MLIB=../minisat/build/install/lib
 MPREFIX=install
 
 make config MINISAT_INCLUDE=-I$MINC
